16c508bf07b40c206b667b39c34bc745135ec1fc
FrancyG8 <55449605+FrancyG8@users.noreply.github.com>
2020-01-28 17:02:11 +0100

    Prova

--- js/sketch-factwrong1.js
+++ js/sketch-factwrong1.js
@@ -2,15 +2,15 @@ function preload() {}
 
 function setup() {}
 
-function draw() {
+function draw() {}
+
+function mouseClicked() {
   var url_string = window.location.href;
   var url = new URL(url_string);
 
-  var check = url.searchParams.get("q1")
-
-  nextb.mousePressed = function() {
-      window.open('../q/q2.html?q1=0', '_self');
-  }
+  var check = url.searchParams.get("q1");
+var next = document.getElementById('next1');
+window.open('../q/q2.html?q1=0', '_self');
 }
 
 function windowResized() {
